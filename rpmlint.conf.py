99ec6b221ac30a8c21cda80f262bce85b6494b8d
Vladimir Rusinov <vladimir@greenmice.info>
2015-12-25 18:41:36 +0000

    gdb: Ignore lint warning for devtoolset unversioned-explicit-obsoletes.

--- rpmlint.conf.py
+++ rpmlint.conf.py
@@ -91,3 +91,7 @@ addFilter('letsencrypt.noarch: W: log-files-without-logrotate')
 ## Libcomps
 # URL broken :(
 addFilter('libcomps.spec: W: invalid-url Source0: https://github.com/midnightercz/libcomps/libcomps-0.1.7.tar.gz')
+
+## GDB
+# Obsoletes all versions
+addFilter('gdb.spec:[0-9]+: W: unversioned-explicit-obsoletes devtoolset')
