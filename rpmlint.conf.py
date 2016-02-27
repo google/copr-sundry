e67b4de90e7e0e5dd35f5e6a8d673f611859a465
Vladimir Rusinov <vrusinov@google.com>
2016-02-27 17:12:49 +0000

    perl: Ignore unversioned-explicit-obsoletes lint warnings.

--- rpmlint.conf.py
+++ rpmlint.conf.py
@@ -151,6 +151,7 @@ addFilter('(python3-)?Cython.x86_64: W: non-executable-script /usr/lib64/python[
 ## Perl
 addFilter('perl.spec: W: %ifarch-applied-patch Patch3: perl-5.8.0-libdir64.patch')
 addFilter('perl\.(src|spec): W: invalid-license')
+addFilter('perl.spec:[0-9]+: W: unversioned-explicit-obsoletes')
 
 ## Python-cryptoghraphy
 addFilter('python3?-cryptography-vectors.noarch: W: pem-certificate')
