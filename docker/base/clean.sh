f8651bbcfd16f28c8f1dc9399e48dd315d8b7df3
Vladimir Rusinov <vrusinov@google.com>
2017-03-07 22:07:56 +0000

    docker-python2: cleanup.

---
+++ docker/base/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf /usr/portage/distfiles/
+rm -rf /usr/lib64/python*/test
+rm -rf /usr/lib64/python*/site-packages/portage/tests
+rm -rf /usr/share/gtk-doc/
+rm -rf /usr/share/doc/
+rm -rf /var/tmp/portage
+rm -rf /etc/udev
+rm -rf /etc/terminfo
+rm /etc/sandbox.conf
+rm /etc/wgetrc
+rm -rf /boot
+rm -rf /media
+rm -rf /mnt
+rm -rf /opt
