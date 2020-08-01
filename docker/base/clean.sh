b959003d56e8583fd3ad0521e2b591255521b82a
Vladimir Rusinov <vrusinov@google.com>
2020-08-01 16:09:34 +0100

    Clean more stuff from base

--- docker/base/clean.sh
+++ docker/base/clean.sh
@@ -11,6 +11,7 @@ rm /usr/share/portage/config/make.conf.example
 rm -rf /usr/src
 
 rm -rf /var/tmp/portage
+rm -rf /tmp/*
 rm -rf /var/cache/*
 rm -rf /var/lib/gentoo/news
 rm -rf /var/log/*
@@ -20,9 +21,53 @@ rm -rf /etc/terminfo
 rm -f /etc/sandbox.conf
 rm -f /etc/wgetrc
 rm -rf /etc/skel
+rm /etc/DIR_COLORS
+rm -rf /etc/default
+rm -rf /etc/init.d
+rm /etc/issue
+rm /etc/nanorc
+rm /etc/rc.conf
+rm /etc/sysctl.conf
+rm /etc/xattr.conf
 
+rm -rf /lib/systemd
+
+rm /bin/dmesg
+rm /bin/findmnt
+rm /bin/login
+rm /bin/mount
+rm /bin/umount
+rm /bin/wdctl
+rm /bin/run-parts
 rm /sbin/pivot_root
 rm /sbin/mkswap
+rm /sbin/agetty
+rm /sbin/blkid
+rm /sbin/blockdev
+rm /sbin/findfs
+rm /sbin/fsck.minix
+rm /sbin/fstrim
+rm /sbin/installkernel
+rm /sbin/losetup
+rm /sbin/mkfs.bfs
+rm /sbin/raw
+rm /sbin/swaplabel
+rm /sbin/swapon
+rm /sbin/wipefs
+rm /usr/sbin/rfkill
+rm /usr/sbin/partx
+
+rm /usr/lib/cracklib_dict.hwm
+rm /usr/lib/cracklib_dict.pwd
+rm /usr/lib/cracklib_dict.pwi
+rm -rf /usr/lib/systemd
+
+rm -rf /usr/share/vim
+rm -rf /usr/share/info
+rm -rf /usr/share/locale
+
+rm -rf /var/cache
+
 
 rm -rf /boot
 rm -rf /media
