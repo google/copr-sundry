72bd49801097958b3df9deee893fe4393e39f0e0
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2020-11-11 13:41:56 +0000

    Docker image for netatmo-wunderground-agent

---
+++ docker/netatmo-wunderground-agent/clean.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# TODO: re-enable once rebuilt without readline
+#rm /lib64/libreadline.so.*
+
+rm /bin/ls
+
+rm -rf /etc/kernel
+rm -rf /etc/profile.d
+rm -rf /etc/python-exec
+rm -rf /usr/lib/go/api
+rm -rf /usr/lib64/node_modules/*/node_modules/*/man
+rm -rf /usr/lib64/node_modules/npm/node_modules/sshpk
+rm -rf /usr/share/automake*
+rm -rf /usr/share/gcc-config
+rm -rf /usr/share/gettext-*
+rm -rf /usr/share/libtool
+rm -rf /usr/share/portage
+rm -rf /var/empty
+rm /bin/awk
+rm /bin/chmod
+rm /bin/dirname
+rm /bin/fuser
+rm /bin/passwd
+rm /bin/sleep
+rm /bin/uname
+rm /etc/csh.env
+rm /etc/host.conf
+rm /etc/mtab
+rm /etc/shadow-
+rm /lib64/libBrokenLocale.so.*
+rm /lib64/libhistory.so.*
+rm /lib64/libnss_compat-*.so
+rm /sbin/fsfreeze
+rm /usr/bin/meson
+rm /usr/bin/xzcmp
+rm /usr/lib64/libb2.so.*
+rm /usr/lib64/libgnutls.so.*
+rm /usr/lib64/libnettle.so
+rm /usr/lib64/libtasn1.so.*
+rm /usr/libexec/editor
+rm /usr/sbin/backup-tar
+rm /usr/sbin/dump-remind
+rm /usr/sbin/fixpackages
+rm /usr/sbin/grpconv
+rm /usr/sbin/perl-cleaner
+rm /usr/sbin/readprofile
+rm /usr/sbin/sysctl
+rm /usr/sbin/vigr
+
+rm /bin/clean.sh
+rm /bin/rm
