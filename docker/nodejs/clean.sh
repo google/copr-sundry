5c16de38e19a2715ba2b8ae0c352e788a191d00a
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2020-11-11 13:05:34 +0000

    Add nodejs image

---
+++ docker/nodejs/clean.sh
@@ -0,0 +1,168 @@
+#!/bin/sh
+
+rm -f /bin/chroot
+rm -f /bin/df
+rm -f /bin/lsblk
+rm -f /bin/sed
+rm -f /bin/su
+rm -f /bin/tar
+rm -f /bin/tty
+rm -f /etc/rpc
+rm -f /sbin/blkzone
+rm -f /sbin/chcpu
+rm -f /sbin/ctrlaltdel
+rm -f /sbin/fsck
+rm -f /sbin/getpcaps
+rm -f /sbin/hwclock
+rm -f /sbin/mkfs.minix
+rm -f /sbin/setcap
+rm -f /sbin/sfdisk
+rm -f /sbin/sln
+rm -f /sbin/swapoff
+rm -f /usr/sbin/addpart
+rm -f /usr/sbin/delpart
+rm -f /usr/sbin/groupdel
+rm -rf /ect/bash
+rm -rf /etc/bash
+rm -rf /etc/env.d
+rm -rf /etc/modprobe.d
+rm -rf /etc/portage
+rm -rf /etc/revdep-rebuild
+rm -rf /etc/runlevels
+rm -rf /etc/sandbox.d
+rm -rf /etc/xml
+rm -rf /lib/gentoo
+rm -rf /usr/include
+rm -rf /usr/lib/go-bootstrap
+rm -rf /usr/lib/portage
+rm -rf /usr/lib/python*/site-packages/gentoolkit
+rm -rf /usr/lib/python*/site-packages/mesonbuild
+rm -rf /usr/lib/python*/site-packages/portage
+rm -rf /usr/lib/python*/test
+rm -rf /usr/lib/python3.*
+rm -rf /usr/lib/tmpfiles.d
+rm -rf /usr/lib64/cmake
+rm -rf /usr/lib64/libbz2.so
+rm -rf /usr/lib64/libgnutlsxx.so.*
+rm -rf /usr/lib64/libnettle.so.*
+rm -rf /usr/lib64/libtasn1.so
+rm -rf /usr/lib64/locale
+rm -rf /usr/lib64/perl5
+rm -rf /usr/libexec/coreutils
+rm -rf /usr/libexec/gcc
+rm -rf /usr/libexec/gpg-check-pattern
+rm -rf /usr/libexec/gpg-protect-tool
+rm -rf /usr/libexec/pager
+rm -rf /usr/share/aclocal*
+rm -rf /usr/share/applications
+rm -rf /usr/share/autoconf*
+rm -rf /usr/share/baselayout
+rm -rf /usr/share/binutils-data
+rm -rf /usr/share/bison
+rm -rf /usr/share/elt-patches
+rm -rf /usr/share/eselect
+rm -rf /usr/share/gcc-data
+rm -rf /usr/share/gdb
+rm -rf /usr/share/gettext
+rm -rf /usr/share/gnuconfig
+rm -rf /usr/share/i18n
+rm -rf /usr/share/libgpg-error
+rm -rf /usr/share/man
+rm -rf /usr/share/nano
+rm -rf /usr/share/openpgp-keys
+rm -rf /usr/share/pixmaps
+rm -rf /usr/share/polkit-1
+rm -rf /usr/share/sandbox
+rm -rf /usr/share/tabset
+rm -rf /usr/share/terminfo
+rm -rf /usr/x86_64-pc-linux-gnu
+rm -rf /var/db
+rm -rf /var/lib/gentoo
+rm -rf /var/lib/portage
+rm /bin/attr
+rm /bin/chgrp
+rm /bin/dd
+rm /bin/expr
+rm /bin/gunzip
+rm /bin/mknod
+rm /bin/ping4
+rm /bin/rmdir
+rm /bin/sort
+rm /bin/true
+rm /etc/etc-update.conf
+rm /etc/filesystems
+rm /etc/inputrc
+rm /etc/locale.gen
+rm /etc/login.access
+rm /etc/login.defs
+rm /etc/passwd-
+rm /etc/profile
+rm /etc/shells
+rm /lib/cpp
+rm /lib64/libBrokenLocale-*.so
+rm /lib64/libfdisk.so.*
+rm /lib64/libnss_compat.so.*
+rm /lib64/libresolv-*.so
+rm /sbin/capsh
+rm /usr/bin/bzcmp
+rm /usr/bin/g++*
+rm /usr/bin/localedef
+rm /usr/bin/mpicalc
+rm /usr/bin/msgattrib
+rm /usr/bin/pkgconf
+rm /usr/bin/setarch
+rm /usr/bin/x86_64-pc-linux-gnu-ld*
+rm /usr/bin/xzdiff
+rm /usr/bin/xzegrep
+rm /usr/lib/python-exec/python3.*/2to3
+rm /usr/lib/python-exec/python3.*/archive-conf
+rm /usr/lib/python-exec/python3.*/dispatch-conf
+rm /usr/lib/python-exec/python3.*/ebuild
+rm /usr/lib/python-exec/python3.*/eclean
+rm /usr/lib/python-exec/python3.*/eclean-dist
+rm /usr/lib/python-exec/python3.*/egencache
+rm /usr/lib/python-exec/python3.*/ekeyword
+rm /usr/lib/python-exec/python3.*/emaint
+rm /usr/lib/python-exec/python3.*/emirrordist
+rm /usr/lib/python-exec/python3.*/enalyze
+rm /usr/lib/python-exec/python3.*/env-update
+rm /usr/lib/python-exec/python3.*/equery
+rm /usr/lib/python-exec/python3.*/eshowkw
+rm /usr/lib/python-exec/python3.*/fixpackages
+rm /usr/lib/python-exec/python3.*/glsa-check
+rm /usr/lib/python-exec/python3.*/meson
+rm /usr/lib/python-exec/python3.*/portageq
+rm /usr/lib/python-exec/python3.*/pydoc
+rm /usr/lib/python-exec/python3.*/python-config
+rm /usr/lib/python-exec/python3.*/python3-config
+rm /usr/lib/python-exec/python3.*/quickpkg
+rm /usr/lib/python-exec/python3.*/regenworld
+rm /usr/lib/python-exec/python3.*/revdep-rebuild
+rm /usr/sbin/addgnupghome
+rm /usr/sbin/applygnupgdefaults
+rm /usr/sbin/archive-conf
+rm /usr/sbin/chgpasswd
+rm /usr/sbin/chpasswd
+rm /usr/sbin/dispatch-conf
+rm /usr/sbin/env-update
+rm /usr/sbin/etc-update
+rm /usr/sbin/groupadd
+rm /usr/sbin/groupmod
+rm /usr/sbin/grpck
+rm /usr/sbin/grpunconv
+rm /usr/sbin/ldattach
+rm /usr/sbin/locale-gen
+rm /usr/sbin/newusers
+rm /usr/sbin/pwconv
+rm /usr/sbin/pwunconv
+rm /usr/sbin/regenworld
+rm /usr/sbin/restore-tar
+rm /usr/sbin/rtcwake
+rm /usr/sbin/savelog
+rm /usr/sbin/update-ca-certificates
+rm /usr/sbin/useradd
+rm /usr/sbin/usermod
+rm /usr/sbin/uuidd
+
+# Remove this script:
+rm -rf /bin/clean.sh
