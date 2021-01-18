fd8838a38a2f27ff1f643b153efebecdaa0d43b9
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-01-18 00:16:01 +0000

    Update base-build to allow multile make.conf files

---
+++ docker/base-build/build.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+
+set -e
+set -x
+
+date_v=$(date '+%Y-%m-%d')
+n="base-build"
+
+docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v .
+docker run --rm -it vrusinov/$n:latest
+docker push vrusinov/$n:latest
+docker push vrusinov/$n:$date_v
