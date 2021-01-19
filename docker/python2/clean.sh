c384ae8ad168a8df776bd7563f082dbd93c3db0f
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-01-19 14:20:29 +0000

    Clean more stuff from python2 docker

---
+++ docker/python2/clean.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+rm -f /sbin/fsck.minix
+rm -f /sbin/fstrim
+rm -f /sbin/installkernel
+rm -f /sbin/losetup
+rm -rf /bin/arping
+rm -rf /bin/attr
+rm -rf /bin/chown
+rm -rf /bin/chroot
+rm -rf /bin/dmesg
+rm -rf /bin/egrep
+rm -rf /bin/login
+rm -rf /bin/mount
+rm -rf /bin/nano
+rm -rf /bin/ping4
+rm -rf /bin/run-parts
+rm -rf /bin/su
+rm -rf /boot
+rm -rf /etc/conf.d
+rm -rf /etc/conf.d/pydoc-*
+rm -rf /etc/default
+rm -rf /etc/DIR_COLORS
+rm -rf /etc/fstab
+rm -rf /etc/gai.conf
+rm -rf /etc/init.d
+rm -rf /etc/issue
+rm -rf /etc/issue.logo
+rm -rf /etc/logrotate.d
+rm -rf /etc/modprobe.d
+rm -rf /etc/nanorc
+rm -rf /etc/rc.conf
+rm -rf /etc/rpc
+rm -rf /etc/rsyncd.conf
+rm -rf /etc/sandbox.conf
+rm -rf /etc/skel
+rm -rf /etc/sysctl.conf
+rm -rf /etc/terminfo
+rm -rf /etc/udev
+rm -rf /etc/wgetrc
+rm -rf /etc/xattr.conf
+rm -rf /etc/xinetd.d
+rm -rf /home
+rm -rf /lib/systemd
+rm -rf /media
+rm -rf /mnt
+rm -rf /opt
+rm -rf /root
+rm -rf /sbin/blockdev
+rm -rf /sbin/consoletype
+rm -rf /sbin/ctrlaltdel
+rm -rf /sbin/fdisk
+rm -rf /sbin/findfs
+rm -rf /sbin/getcap
+rm -rf /sbin/getpcaps
+rm -rf /sbin/mkfs
+rm -rf /sbin/setcap
+rm -rf /sbin/sfdisk
+rm -rf /sbin/sln
+rm -rf /tmp/*
+rm -rf /usr/lib/cracklib_dict.hwm
+rm -rf /usr/lib/cracklib_dict.pwd
+rm -rf /usr/lib/cracklib_dict.pwi
+rm -rf /usr/lib/python3*/
+rm -rf /usr/lib/systemd
+rm -rf /usr/lib64/locale
+rm -rf /usr/lib64/python*/site-packages/allmydata/test
+rm -rf /usr/lib64/python*/site-packages/portage/tests
+rm -rf /usr/lib64/python*/site-packages/twisted/test
+rm -rf /usr/lib64/python*/test
+rm -rf /usr/local
+rm -rf /usr/portage/distfiles/
+rm -rf /usr/sbin/delpart
+rm -rf /usr/sbin/logoutd
+rm -rf /usr/sbin/partx
+rm -rf /usr/sbin/resizepart
+rm -rf /usr/sbin/rfkill
+rm -rf /usr/share/applications
+rm -rf /usr/share/bash-completion
+rm -rf /usr/share/doc/
+rm -rf /usr/share/gtk-doc/
+rm -rf /usr/share/info
+rm -rf /usr/share/locale
+rm -rf /usr/share/man
+rm -rf /usr/share/nano
+rm -rf /usr/share/portage/config/make.conf.example
+rm -rf /usr/share/vim
+rm -rf /usr/share/zsh
+rm -rf /usr/src
+rm -rf /var/cache
+rm -rf /var/cache/*
+rm -rf /var/lib/gentoo/news
+rm -rf /var/log/*
+rm -rf /var/tmp/portage
+rm /bin/findmnt
+rm /bin/umount
+rm /bin/wdctl
+rm /sbin/agetty
+rm /sbin/blkdiscard
+rm /sbin/blkid
+rm /sbin/blkzone
+rm /sbin/fsck
+rm /sbin/mkfs.bfs
+rm /sbin/mkfs.minix
+rm /sbin/mkswap
+rm /sbin/pivot_root
+rm /sbin/raw
+rm /sbin/sulogin
+rm /sbin/swaplabel
+rm /sbin/swapon
+rm /sbin/switch_root
+rm /sbin/wipefs
+rm /sbin/zramctl
+rm /usr/sbin/addpart
