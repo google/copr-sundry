2b5c3f504da4384b6cd08b652aeef7cc127378fb
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-05-09 15:52:23 +0100

    Add java docker image

---
+++ docker/java/README.md
@@ -0,0 +1,4 @@
+# Java docker image
+
+This image contains JDK, and intended to be used as a base layer for java-based
+application images.
---
+++ docker/java/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -f /usr/bin/ctest
+rm -rf /usr/lib/python*
+rm -rf /usr/lib64/perl5
+
+# Remove this script:
+rm -rf /usr/local
