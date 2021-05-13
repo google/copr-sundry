167d1f49637ec7d6e7addbd7b06543e4eed95556
Vladimir Rusinov <vrusinov@google.com>
2021-05-13 21:40:59 +0100

    Docker image for the Tsunami Security Scanner (#218)

---
+++ docker/tsunami-security-scanner/README.md
@@ -0,0 +1,9 @@
+# Tsunami Security Scanner
+
+Docker image for the [Tsunami Security Scanner](https://github.com/google/tsunami-security-scanner)
+
+Usage example:
+
+```bash
+docker run -e "IP_V4_TARGET=127.0.0.1" --rm -it vrusinov/tsunami-security-scanner:latest
+```
---
+++ docker/tsunami-security-scanner/build.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+
+set -e
+set -x
+
+date_v=$(date '+%Y-%m-%d')
+v="0.0.4"
+n="tsunami-security-scanner"
+
+mkdir -p /tmp/docker-build
+
+docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
+#docker run -e "IP_V4_TARGET=127.0.0.1" --rm -it vrusinov/$n:latest
+#exit 0
+docker push vrusinov/$n:latest
+docker push vrusinov/$n:$date_v
+docker push vrusinov/$n:$v
+docker push vrusinov/$n:$v.$date_v
