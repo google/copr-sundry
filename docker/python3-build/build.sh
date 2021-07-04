3566f55313675c005761b86cf8f843ef67b7a91e
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-07-04 21:06:25 +0100

    Tag python-build with 3.8 tag too

--- docker/python3-build/build.sh
+++ docker/python3-build/build.sh
@@ -4,10 +4,11 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v=3.9.5
+v=3.8.10
+v_short=3.8
 n="python3-build"
 
-docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
+docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v -t vrusinov/$n:$v_short -t vrusinov/$n:$v_short.$date_v .
 docker run --rm -it vrusinov/$n:latest
 docker push vrusinov/$n:latest
 docker push vrusinov/$n:$date_v
