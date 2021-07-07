f63ae414a2bf9b04e07941337720c305c604414c
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-07-07 18:12:10 +0100

    Update django

--- docker/django/build.sh
+++ docker/django/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v="3.2.4"
+v="3.2.5"
 n="django"
 
 docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
