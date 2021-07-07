f0991a00c0130deb63d927abb946974b87792742
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-07-07 17:53:56 +0100

    Update python3 image

--- docker/python3/clean.sh
+++ docker/python3/clean.sh
@@ -2,6 +2,8 @@
 
 rm -rf /usr/lib/gcc/x86_64-pc-linux-gnu/*/include
 rm -rf /usr/lib/gcc/x86_64-pc-linux-gnu/*/plugin/include
+rm -rf /usr/lib/python*/site-packages/mesonbuild
 rm -rf /usr/lib/python*/site-packages/portage
-rm -rf /usr/lib/python3.7/__pycache__
-rm -rf /usr/lib/python3.9/__pycache__
+rm -rf /usr/lib/python3.7/
+rm -rf /usr/lib/python3.9/
+rm -rf /usr/lib64/perl5
