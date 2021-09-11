87555a67f865d646078e7d0cd90a282c75df2ab1
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-09-11 17:40:33 +0100

    Update java-build

--- docker/java-build/build.sh
+++ docker/java-build/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v="8.272"
+v="1.8.0_292"
 n="java-build"
 
 docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
