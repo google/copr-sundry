9fc2d196262fdb355d14cd2af11af4dc83275f18
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-02-27 17:16:57 +0000

    Update django

--- docker/django/clean.sh
+++ docker/django/clean.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/libstdc++.a
+rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/libstdc++fs.a
 rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/plugin/gtype.state
 rm -f /usr/lib64/gconv/BIG5HKSCS.so
 rm -rf /usr/lib64/perl5
