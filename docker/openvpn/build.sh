604f3dd7f0aea381c2b50ba4706c46c04b55a157
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-03-04 22:48:42 +0000

    Add iptables to OpenVPn docker

--- docker/openvpn/build.sh
+++ docker/openvpn/build.sh
@@ -5,11 +5,14 @@ set -x
 
 date_v=$(date '+%Y-%m-%d')
 v=2.5.2
+vr=2.5.2-r1
 n="openvpn"
 
-docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
+docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$vr -t vrusinov/$n:$v.$date_v  -t vrusinov/$n:$vr.$date_v .
 docker run --rm -it vrusinov/$n:latest
 docker push vrusinov/$n:latest
 docker push vrusinov/$n:$date_v
 docker push vrusinov/$n:$v
+docker push vrusinov/$n:$vr
 docker push vrusinov/$n:$v.$date_v
+docker push vrusinov/$n:$vr.$date_v
