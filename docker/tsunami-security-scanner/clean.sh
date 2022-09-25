5e2c553adf8b63fd83c0627aa57bf286505f5833
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-09-25 21:43:01 +0100

    Update tsunami to 0.0.14

--- docker/tsunami-security-scanner/clean.sh
+++ docker/tsunami-security-scanner/clean.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-rm -f /usr/lib64/gconv/BIG5*.so
 rm -f /usr/lib64/gconv/IBM*.so
+rm -r /etc/udev
+rm -r /usr/share/doc
 rm -rf /usr/lib64/binutils/
+rm /usr/lib64/gconv/BIG5*.so
+rm /usr/lib64/gconv/libJISX0213.so
 
 # Clean this script
 rm -f /usr/local/bin/clean.sh
