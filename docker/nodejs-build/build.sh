6089c59791a9fa49b8810c672e7c3acc48c495a6
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-23 19:51:24 +0000

    Update nodejs

--- docker/nodejs-build/build.sh
+++ docker/nodejs-build/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v="14.16.1"
+v="18.12.1"
 n="nodejs-build"
 
 docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
