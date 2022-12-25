01ea5fe1c5fe9a17bc4355b61baf0226c9e14358
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-25 22:47:22 +0000

    Create python:3.10 docker image

--- docker/python310/clean.sh
+++ docker/python310/clean.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-rm -r /usr/lib/gcc/x86_64-pc-linux-gnu/*/include
 rm -r /usr/lib/gcc/x86_64-pc-linux-gnu/*/libgcc.a
 rm -r /usr/lib/gcc/x86_64-pc-linux-gnu/*/plugin
-rm -r /usr/lib/gcc/x86_64-pc-linux-gnu/*/plugin/include
 rm -r /usr/lib/python*/site-packages/gentoolkit*
 rm -r /usr/lib/python*/site-packages/mesonbuild*
 rm -r /usr/lib/python*/site-packages/portage*
+rm -r /usr/lib/python3.*/test
 rm -r /usr/lib/python3.7/
 rm -r /usr/lib/python3.8/
-rm -r /usr/lib/python3.9/test
 rm -r /usr/lib64/perl5
+rm /bin/vdir
+rm /usr/lib/gcc/x86_64-pc-linux-gnu/*/libitm.a
 rm /usr/lib/gcc/x86_64-pc-linux-gnu/*/libstdc++fs.a
 rm /usr/lib/gcc/x86_64-pc-linux-gnu/*/libsupc++.a
