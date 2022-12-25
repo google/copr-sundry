5f7ea3df693100a0347f4721a4a4502e5c67e058
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-25 17:05:08 +0000

    Update base docker

--- docker/base-build/build.sh
+++ docker/base-build/build.sh
@@ -9,7 +9,7 @@ n="base-build"
 docker pull gentoo/portage:latest
 docker pull gentoo/stage3:amd64-nomultilib-systemd
 
-docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v .
+docker build --no-cache -t vrusinov/$n:latest -t vrusinov/$n:$date_v .
 docker run --rm -it vrusinov/$n:latest
 docker push vrusinov/$n:latest
 docker push vrusinov/$n:$date_v
