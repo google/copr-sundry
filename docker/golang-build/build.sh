3ab6cd734f06bd118506598e38bdec26ddf40d22
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-25 14:42:48 +0000

    Update golang

--- docker/golang-build/build.sh
+++ docker/golang-build/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v="1.18.2"
+v="1.19.4"
 n="golang-build"
 
 docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
