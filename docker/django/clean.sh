3cce9e3236387ac4267366c7089c6f1fcf8e5d28
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-26 00:17:42 +0000

    Update django docker

--- docker/django/clean.sh
+++ docker/django/clean.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/libquadmath.a
-rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/libstdc++fs.a
-rm -f /usr/lib/gcc/x86_64-pc-linux-gnu/*/plugin/gtype.state
 rm -f /usr/lib64/gconv/BIG5HKSCS.so
 rm -f /usr/lib64/gconv/IBM1399.so
+rm -r /usr/lib/python3.9/site-packages/Cython
+rm -r /usr/lib/python3.9/site-packages/pydantic
+rm -r /usr/lib/python3.9/site-packages/setuptools
 rm -rf /usr/lib64/perl5
 rm -rf /usr/share/sgml/
-rm /lib64/libsystemd.so.0.32.0
-rm /usr/lib64/gconv/GB18030.so
-rm /usr/lib64/gconv/IBM1390.so
